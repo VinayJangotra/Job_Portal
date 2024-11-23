f310de6cefccaf99d16a7a6e8f830a03c28b15c5
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-11-23 11:34:31 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,13 @@ import { errorMiddleware } from './middlewares/error.js';
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 // add the methods that we want to allow cross-origin access to our API from other origins
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+app.use(
+  cors({
+    origin: true, // Allow requests from any origin
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials:true,
-}));
+    credentials: true,
+  })
+);
 app.use(cookieParser()); // parse cookies
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true})); // for parsing application/x-www-form-urlencoded
